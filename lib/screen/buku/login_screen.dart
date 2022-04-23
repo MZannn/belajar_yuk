75195718001501d2fb653b299d0e1f2a7b459e17
MZannn <mfauzan1098@gmail.com>
2022-04-24 04:43:49 +0700

    Feature Login

--- lib/screen/buku/login_screen.dart
+++ lib/screen/buku/login_screen.dart
@@ -15,7 +15,16 @@ class LoginScreen extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Image.network("https://picsum.photos/id/870/200"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
                   controller: emailCtrl,
                   decoration: const InputDecoration(
@@ -29,6 +38,9 @@ class LoginScreen extends StatelessWidget {
                     }
                     return null;
                   },
+                ),
+                const SizedBox(
+                  height: 10,
                 ),
                 TextFormField(
                   controller: passwordCtrl,
@@ -44,7 +56,26 @@ class LoginScreen extends StatelessWidget {
                     return null;
                   },
                 ),
-                ElevatedButton(onPressed: () {}, child: const Text('Submit'))
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _formKey.currentState!.save();
+                          }
+                        },
+                        child: const Text('Log in')),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {}, child: const Text('Register')),
+                  ],
+                )
               ],
             ),
           )),
